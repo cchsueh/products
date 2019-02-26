a66a1e6910045e9bd63c4e2c8e4ad3a3b1dc882e
Your Namecchsueh <md1004000@gmail.com>
2019-02-26 22:48:14 +0800

    add for loop for printing product

--- products.py
+++ products.py
@@ -1,8 +1,11 @@
 products = []
 while True:
-    name = input('請輸入商品名稱: ')
+    name = input('請輸入商品名稱： ')
     if name == 'q':
         break
-    price = input('請輸入商品價錢: ')
+    price = input('請輸入商品價錢： ')
     products.append([name, price])
 print(products)
+
+for p in products:
+    print(p[0], '的價格： ', p[1])
