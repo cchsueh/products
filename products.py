bc6d1dce7587fc062b21c3039132b254f962fcf4
Your Namecchsueh <md1004000@gmail.com>
2019-03-01 15:50:02 +0800

    add check file

--- products.py
+++ products.py
@@ -1,12 +1,19 @@
-# 讀取檔案
+import os #operating system
+
 products = []
-with open('products.csv', 'r', encoding='utf-8') as f:
-    for line in f:
-        if '商品,價格' in line:
-            continue
-        name, price = line.strip().split(',')
-        products.append([name, price])
-print(products)
+# 確認檔案是否存在
+if os.path.isfile('products.csv'):
+    print('找到檔案了!!')
+    # 讀取檔案
+    with open('products.csv', 'r', encoding='utf-8') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    print(products)
+else:
+    print('找不到檔案......')
 
 # 輸入購買紀錄
 while True:
